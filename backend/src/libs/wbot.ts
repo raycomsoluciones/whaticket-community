73ffacf5ca37deff8cd91df0f6fd1ba5dfc96610
raycomsoluciones <43359064+raycomsoluciones@users.noreply.github.com>
2025-10-23 20:36:12 -0500

    Update wbot.ts

--- backend/src/libs/wbot.ts
+++ backend/src/libs/wbot.ts
@@ -43,18 +43,25 @@ export const initWbot = async (whatsapp: Whatsapp): Promise<Session> => {
         sessionCfg = JSON.parse(whatsapp.session);
       }
 
-      const args:String = process.env.CHROME_ARGS || "";
+      const puppeteerArgs = [
+  '--no-sandbox',
+  '--disable-setuid-sandbox',
+  '--disable-dev-shm-usage',
+  '--disable-accelerated-2d-canvas',
+  '--no-first-run',
+  '--no-zygote',
+  '--single-process',
+  '--disable-gpu'
+];
 
-      const wbot: Session = new Client({
-        session: sessionCfg,
-        authStrategy: new LocalAuth({clientId: 'bd_'+whatsapp.id}),
-        puppeteer: {
-          executablePath: process.env.CHROME_BIN || undefined,
-          // @ts-ignore
-          browserWSEndpoint: process.env.CHROME_WS || undefined,
-          args: args.split(' ')
-        }
-      });
+const wbot: Session = new Client({
+  session: sessionCfg,
+  authStrategy: new LocalAuth({ clientId: 'bd_' + whatsapp.id }),
+  puppeteer: {
+    executablePath: process.env.CHROME_BIN || '/usr/bin/google-chrome-stable',
+    args: puppeteerArgs
+  }
+});
 
       wbot.initialize();
 
